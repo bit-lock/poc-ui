cb68715b05e433f9dc5b881222fd90ec930d6f4f
afarukcali <afarukcali@gmail.com>
2022-10-25 15:10:17 +0300

    User vault list show.

--- src/pages/Vaults.tsx
+++ src/pages/Vaults.tsx
@@ -1,7 +1,44 @@
 import React, { useEffect } from "react";
+import { Web3Lib } from "../lib/Web3Lib";
 
 export const Vaults = () => {
-  useEffect(() => {}, []);
+  useEffect(() => {
+    const init = async () => {
+      const web3Instance = new Web3Lib();
+
+      const vaultCount = await web3Instance.getVaultLength();
+      const fetchVaultPromises = [];
+      const fetchSignatoriesPromises = [];
+
+      for (let i = 0; i < vaultCount; i++) {
+        fetchVaultPromises.push(web3Instance.getVaults(i));
+        fetchSignatoriesPromises.push(web3Instance.getSignatories(i));
+      }
+
+      const vaults = await Promise.all(fetchVaultPromises);
+      const signatories = await Promise.all(fetchSignatoriesPromises);
+
+      const myAddresses: string[] = await window.ethereum.request({ method: "eth_requestAccounts" });
+      const myCurrentAdress = myAddresses[0].toLowerCase();
+
+      const accountVaultList = [];
+
+      for (let z = 0; z < vaultCount; z++) {
+        const currentVault = vaults[z];
+        const currentSignatories = signatories[z][0].map((data: string) => data.toLowerCase());
+
+        if (currentVault.initiator.toLowerCase() === myCurrentAdress) {
+          accountVaultList.push({ vault: vaults[z], signatories: currentSignatories, isMyOwner: true });
+        } else if (currentSignatories.includes(myCurrentAdress)) {
+          accountVaultList.push({ vault: vaults[z], signatories: currentSignatories, isMyOwner: false });
+        }
+      }
+
+      console.log(accountVaultList);
+    };
+
+    init();
+  }, []);
 
   return <span>Vaults</span>;
 };
