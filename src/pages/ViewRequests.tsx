354d533123906675434b6d7f6d287f6ae620a5fc
reyhanboydag <reyhanboydag@gmail.com>
2022-10-26 14:26:15 +0300

    view request page design

--- src/pages/ViewRequests.tsx
+++ src/pages/ViewRequests.tsx
@@ -1,5 +1,185 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Button, Container, Content, FlexboxGrid, Loader, Panel } from "rsuite";
+import styled from "styled-components";
+import { Web3Lib } from "../lib/Web3Lib";
 
-export const ViewRequests = () => {
-  return <span>View requests</span>;
+type Props = {
+  account: string;
+  publicKey: string;
 };
+
+export const ViewRequests: React.FC<Props> = ({ account, publicKey }) => {
+  const [loading, setLoading] = useState<boolean>(true);
+  const [approveRequestList, setApproveRequestList] = useState<Array<any>>([]);
+  const [finalizeRequestList, setFinalizeRequestList] = useState<Array<any>>([]);
+
+  useEffect(() => {
+    const init = async () => {
+      const web3Instance = new Web3Lib();
+
+      const vaultCount = await web3Instance.getVaultLength();
+      const fetchVaultPromises = [];
+      const fetchSignatoriesPromises = [];
+
+      for (let i = 0; i < vaultCount; i++) {
+        fetchVaultPromises.push(web3Instance.getVaults(i));
+        fetchSignatoriesPromises.push(web3Instance.getSignatories(i));
+      }
+
+      const vaults = await Promise.all(fetchVaultPromises);
+      const signatories = await Promise.all(fetchSignatoriesPromises);
+
+      const myCurrentAdress = account.toLowerCase();
+
+      const accountVaultList = [];
+
+      for (let z = 0; z < vaultCount; z++) {
+        const currentVault = vaults[z];
+        const currentSignatories = signatories[z][0].map((data: string) => data.toLowerCase());
+
+        if (currentVault.initiator.toLowerCase() === myCurrentAdress) {
+          accountVaultList.push({ id: z, vault: vaults[z], signatories: signatories[z], isMyOwner: true });
+        } else if (currentSignatories.includes(myCurrentAdress)) {
+          accountVaultList.push({ id: z, vault: vaults[z], signatories: signatories[z], isMyOwner: false });
+        }
+      }
+
+      const notFinalizedVaults = accountVaultList.filter((data) => data.vault.status === "0x00");
+
+      const approveList = notFinalizedVaults.filter((currentData) => {
+        const signatoryAddressList = currentData.signatories[0];
+        const myCurrentIndex = signatoryAddressList.findIndex((address: string) => address.toLowerCase() === myCurrentAdress);
+        const signatoryPubKeyList = currentData.signatories[2];
+
+        if (signatoryPubKeyList[myCurrentIndex] !== "0x0000000000000000000000000000000000000000000000000000000000000000") {
+          return false;
+        } else {
+          return true;
+        }
+      });
+
+      setApproveRequestList(approveList);
+
+      const waitingFinalizeList = notFinalizedVaults.filter((data) => {
+        if (data.isMyOwner) {
+          const signatories = data.signatories[2];
+          console.log(signatories, "signatories");
+          return !signatories.some((element: any) => {
+            return element === "0x0000000000000000000000000000000000000000000000000000000000000000";
+          });
+        } else {
+          return false;
+        }
+      });
+
+      setFinalizeRequestList(waitingFinalizeList);
+      setLoading(false);
+    };
+
+    init();
+  }, [account]);
+
+  const approveSignatory = async (id: number) => {
+    setLoading(true);
+    const web3Instance = new Web3Lib();
+    await web3Instance.approveSignatory(id, "0x" + publicKey, account);
+    setLoading(false);
+  };
+
+  const finalizeVault = async (id: number) => {
+    setLoading(true);
+    const web3Instance = new Web3Lib();
+    await web3Instance.finalizeVault(id, account);
+    setLoading(false);
+  };
+
+  if (loading) {
+    return <Loader backdrop content="Fetching requests..." vertical />;
+  }
+
+  return (
+    <Container style={{ backgroundColor: "blue", height: "100vh" }}>
+      <Content>
+        <StyledBox justify="center">
+          <StyledBoxItem colspan={12}>
+            <Panel>
+              <Text>Vault Approve Requests</Text>
+              <br />
+              {approveRequestList.map((data) => {
+                return (
+                  <RequestItem key={data.id}>
+                    <Text>{data.vault.name || "Reyhan"}</Text>
+                    <ButtonGroup>
+                      <Button appearance="link" active color="blue" onClick={() => approveSignatory(data.id)}>
+                        Accept
+                      </Button>
+                      <Button appearance="link" active color="red">
+                        Decline
+                      </Button>
+                    </ButtonGroup>
+                  </RequestItem>
+                );
+              })}
+
+              <Text>Vault Finalize Requests</Text>
+              <br />
+
+              {finalizeRequestList.map((data) => {
+                return (
+                  <RequestItem key={data.id}>
+                    <Text>{data.vault.name}</Text>
+                    <ButtonGroup>
+                      <Button appearance="link" active color="blue" onClick={() => finalizeVault(data.id)}>
+                        Finalize
+                      </Button>
+                    </ButtonGroup>
+                  </RequestItem>
+                );
+              })}
+            </Panel>
+          </StyledBoxItem>
+        </StyledBox>
+      </Content>
+    </Container>
+  );
+};
+
+interface TextProps {
+  fontSize?: string;
+  alignSelf?: string;
+}
+
+const StyledBox = styled(FlexboxGrid)`
+  position: absolute;
+  top: 0;
+  bottom: 0;
+  width: 100%;
+  //background-color: blue;
+`;
+
+const Text = styled.span<TextProps>`
+  font-size: ${(props) => (props.fontSize ? props.fontSize : "16px")};
+  font-weight: 600;
+  color: #6c6b6b;
+  align-self: center;
+`;
+
+const RequestItem = styled.div`
+  border: none;
+  width: 100%;
+  background: #d0cece;
+  height: 50px;
+  display: flex;
+  align-items: center;
+  padding: 1rem;
+  margin: 0.5rem 0;
+  justify-content: space-between;
+`;
+
+const StyledBoxItem = styled(FlexboxGrid.Item)`
+  margin: auto;
+  background: white;
+  border-radius: 8px;
+`;
+
+const ButtonGroup = styled.div``;
