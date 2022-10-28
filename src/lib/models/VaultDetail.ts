efef32bc181edde44a15276ef732af5ec8fab1ab
reyhanboydag <reyhanboydag@gmail.com>
2022-10-28 17:44:33 +0300

    Vault Detail model created.

---
+++ src/lib/models/VaultDetail.ts
@@ -0,0 +1,12 @@
+export type VaultDetail = {
+  0: string;
+  1: string;
+  2: string;
+  3: string;
+  4: string;
+  initiator: string;
+  name: string;
+  status: string;
+  threshold: string;
+  totalShare: string;
+};
