30bd8a07f21e8eff2517136445ce80a5622a2d7d
afarukcali <afarukcali@gmail.com>
2022-11-08 16:09:35 +0300

    utxo fetch for bitcoin testnet

--- src/lib/bitcoin/utils.ts
+++ src/lib/bitcoin/utils.ts
@@ -2,11 +2,61 @@ import { UTXO } from "../models/UTXO";
 import axios from "axios";
 import { RecommendedFee } from "../models/RecommendedFee";
 import WizData from "@script-wiz/wiz-data";
+import { esploraClient, init, TxDetail } from "@bitmatrix/esplora-api-client";
 
 const recomommendedFee = async () => {
   return axios.get<RecommendedFee>("https://mempool.space/api/v1/fees/recommended").then((response) => {
     return response.data;
   });
+};
+
+export const fetchUtxos = async (address: string): Promise<UTXO[]> => {
+  init("https://blockstream.info/testnet/api");
+
+  let myUtxoSets: UTXO[] = [];
+  let allTxs: TxDetail[] = [];
+
+  try {
+    allTxs = await esploraClient.addressTxs(address);
+  } catch (err: any) {
+    toastr.error(err.response.data);
+  }
+
+  const confirmedTxs = allTxs.filter((tx) => tx.status.confirmed);
+
+  if (confirmedTxs.length > 0) {
+    const myPromises = confirmedTxs.map((tx) => {
+      return esploraClient.txOutspends(tx.txid);
+    });
+
+    return Promise.all(myPromises).then((myProm) => {
+      myProm.forEach((os, index) => {
+        const tx = confirmedTxs[index];
+
+        const unSpentIndexs = os
+          .map((outspend, index: number) => {
+            if (!outspend.spent) {
+              return index;
+            }
+          })
+          .filter((dt) => dt !== undefined);
+
+        if (unSpentIndexs.length > 0) {
+          unSpentIndexs.forEach((us) => {
+            if (us !== undefined) {
+              if (tx.vout[us].scriptpubkey_address === address) {
+                myUtxoSets.push({ txId: tx.txid, vout: us, value: (tx.vout[us].value || 0) / 100000000 });
+              }
+            }
+          });
+        }
+      });
+
+      return myUtxoSets;
+    });
+  }
+
+  return myUtxoSets;
 };
 
 export const calculateTxFees = async (utxos: UTXO[], minimumSignatoryCount: number, template: string) => {
